af18ba3babc1c1f0fed9145895588858885836eb
mitchelldschafer <100163566+mitchelldschafer@users.noreply.github.com>
2025-07-01 20:58:16 -0600

    Commit from Bolt to GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ZorlexAI-Site/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
